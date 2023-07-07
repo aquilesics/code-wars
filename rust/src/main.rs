ac2995cc653a8f307130a9e9b01ebc9e314f4d56
aquilesics <aquiles.cout@gmail.com>
2023-07-07 00:25:07 -0300

    credit card mask

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,28 @@
 
 fn main() {
-    let mut _v1 = "AAAABBBCCDAABBB".chars();
-    let mut _v2 = "ABBCcAD".chars();
-    let mut _v3 = vec![1,2,2,3,3];
+    // let mut _v1 = "AAAABBBCCDAABBB".chars();
+    // let mut _v2 = "ABBCcAD".chars();
+    // let mut _v3 = vec![1,2,2,3,3];
 
-    assert_eq!(unique_in_order(_v1), vec!['A','B','C','D','A','B']);
-    assert_eq!(unique_in_order(_v2), vec!['A','B','C','c','A','D']);
-    assert_eq!(unique_in_order(_v3), vec![1,2,3])
+    // assert_eq!(unique_in_order(_v1), vec!['A','B','C','D','A','B']);
+    // assert_eq!(unique_in_order(_v2), vec!['A','B','C','c','A','D']);
+    // assert_eq!(unique_in_order(_v3), vec![1,2,3])
+
+
+    assert_eq!(maskify("4556364607935616"), "############5616");
+    assert_eq!(maskify("1"), "1");
+    assert_eq!(maskify("11111"), "#1111");
 
 }
+
+#[allow(dead_code)]
+fn maskify(cc: &str) -> String {
+    let mut a =  cc.to_string() ;
+    let r = if &a.len() > &4 { &a.len() -4 } else {0};
+    a.replace_range(0..r, &"#".repeat(r) );
+    a
+}
+
 
 #[allow(dead_code)]
 fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
